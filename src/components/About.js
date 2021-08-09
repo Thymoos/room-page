7abd4dbaa7b8d8996f79df9c348b24c2b24eafda
Konrad Izdebski <konradizdebski1011@gmail.com>
2021-08-09 13:40:57 +0200

    Add styles to About component

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ const About = () => {
         <div className="about">
             <img src={imageAboutDark} alt="dark furniture" />
             <div className="text">
-                <h1>ABOUT OUR FURNITURE</h1>
+                <h2>ABOUT OUR FURNITURE</h2>
                 <p>Our multifunctional collection blends design and function to suit your individual taste.
                     Make each room unique, or pick a cohesive theme that best express your interests and what
                     inspires you. Find the furniture pieces you need, from traditional to contemporary styles

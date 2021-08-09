b5fa7b2c81855fa326a8c1d325cf5f4c011c8c0d
Konrad Izdebski <konradizdebski1011@gmail.com>
2021-08-09 13:29:36 +0200

    Add styles to slider component

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -17,7 +17,7 @@ const Slider = () => {
                 Our experts combine form and function in bringing your vision to life. Create a room in your 
                 own style with our collection and make your property a reflection of you and what you love.
                 </p>
-                <button>SHOP NOW </button>
+                <button>SHOP NOW</button>
             </div>
         </div>
      );

4e63b1a102f568549c84434c325df5e182a30745
Konrad Izdebski <konradizdebski1011@gmail.com>
2021-08-10 14:06:44 +0200

    Add image and article change

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,22 +1,67 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Slider.css';
 import img1 from '../images/desktop-image-hero-1.jpg';
+import img2 from '../images/desktop-image-hero-2.jpg';
+import img3 from '../images/desktop-image-hero-3.jpg';
+
+const slides = [
+     {
+        id: 0,
+        h1: "Discover innovative ways to decorate",
+        text: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
+        img: img1
+    },
+    {
+        id: 1,
+        h1: "We are available all across the globe",
+        text: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
+        img: img2
+    },
+    {
+        id: 2,
+        h1: "Manufactured with the best materials",
+        text: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
+        img: img3
+    }
+]
 
 const Slider = () => {
+
+    const [slide, setSlide] = useState(slides[0]);
+
+    const handleChangeLeft = () =>{
+        const id = slide.id-1;
+
+        if(id === -1){
+            setSlide(slides[2]);
+        }
+        else{
+            setSlide(slides[id]);
+        }
+    }
+
+    const handleChangeRight = () =>{
+        const id = slide.id+1;
+        
+        if(id === 3){
+            setSlide(slides[0]);
+        }
+        else{
+            setSlide(slides[id]);
+        }
+    }
+
     return ( 
         <div className="slider">
-            <img src={img1} alt="our furniture" />
+            <img src={slide.img} alt="our furniture" />
             <div className="arrows">
-                <button>{"<"}</button>
-                <button>&gt;</button>
+                <button onClick={handleChangeLeft}>{"<"}</button>
+                <button onClick={handleChangeRight}>&gt;</button>
             </div>
 
             <div className="info">
-                <h1>Discover innovative ways to decorate</h1>
-                <p>We provide unmatched quality, comfort, and style for property owners across the country. 
-                Our experts combine form and function in bringing your vision to life. Create a room in your 
-                own style with our collection and make your property a reflection of you and what you love.
-                </p>
+                <h1>{slide.h1}</h1>
+                <p>{slide.text}</p>
                 <button>SHOP NOW</button>
             </div>
         </div>

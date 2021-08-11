97b1f00e7c9597afd7bed68fce8923c1cbe70039
Konrad Izdebski <konradizdebski1011@gmail.com>
2021-08-11 14:29:21 +0200

    Add image resolution change function

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './Slider.css';
 import img1 from '../images/desktop-image-hero-1.jpg';
 import img2 from '../images/desktop-image-hero-2.jpg';
@@ -6,31 +6,46 @@ import img3 from '../images/desktop-image-hero-3.jpg';
 import arrowLeft from '../images/icon-angle-left.svg';
 import arrowRight from '../images/icon-angle-right.svg';
 import arrow from '../images/icon-arrow.svg';
+import mobileImg1 from '../images/mobile-image-hero-1.jpg';
+import mobileImg2 from '../images/mobile-image-hero-2.jpg';
+import mobileImg3 from '../images/mobile-image-hero-3.jpg';
 
 const slides = [
      {
         id: 0,
         h1: "Discover innovative ways to decorate",
         text: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
-        img: img1
+        imgDesktop: img1,
+        imgMobile: mobileImg1
     },
     {
         id: 1,
         h1: "We are available all across the globe",
         text: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
-        img: img2
+        imgDesktop: img2,
+        imgMobile: mobileImg2
     },
     {
         id: 2,
         h1: "Manufactured with the best materials",
         text: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
-        img: img3
+        imgDesktop: img3,
+        imgMobile: mobileImg3
     }
 ]
+
+function getWindowDimensions() {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+      width,
+      height
+    };
+  }
 
 const Slider = () => {
 
     const [slide, setSlide] = useState(slides[0]);
+    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
 
     const handleChangeLeft = () =>{
         const id = slide.id-1;
@@ -54,9 +69,19 @@ const Slider = () => {
         }
     }
 
+    useEffect(() => {
+        function handleResize() {
+          setWindowDimensions(getWindowDimensions());
+        }
+    
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+      }, []);
+    
+
     return ( 
         <div className="slider">
-            <img src={slide.img} alt="our furniture" />
+            <img src={windowDimensions.width <= 500 ? slide.imgMobile : slide.imgDesktop} alt="our furniture" />
             <div className="arrows">
                 <button onClick={handleChangeLeft}><img src={arrowLeft} alt="" /></button>
                 <button onClick={handleChangeRight}><img src={arrowRight} alt="" /></button>

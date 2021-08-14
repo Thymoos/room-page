2361c9b1b448bd1681eef96a0bc35eb8a5e27e28
Konrad Izdebski <konradizdebski1011@gmail.com>
2021-08-14 13:15:53 +0200

    Css for mobile tweaks

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -23,10 +23,10 @@ const Menu = () => {
             <nav>
                 <ul>
                     <li>room</li>
-                    <li><a href="#">home</a></li>
-                    <li><a href="#">shop</a></li>
-                    <li><a href="#">about</a></li>
-                    <li><a href="#">contact</a></li>
+                    <li><a>home</a></li>
+                    <li><a>shop</a></li>
+                    <li><a>about</a></li>
+                    <li><a>contact</a></li>
                 </ul>
             </nav>
             <div className="shade"></div>
